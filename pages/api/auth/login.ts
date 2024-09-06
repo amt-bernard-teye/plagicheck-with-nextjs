dc34c000c08e06da79b9f3ed0603b28f381c8e2a
Bernard Teye <bernard.teye@amalitech.com>
2024-09-06 13:15:17 +0000

    add login.ts to respond to api/auth/login route

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { compare } from "bcryptjs";
+import * as cookie from "cookie";
+import { sign } from "jsonwebtoken";
+import * as Yup from "yup";
+
+import { StatusCode } from "@/lib/enums/status-code";
+import { UserRepository } from "@/lib/repository/user.repository";
+
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required").trim(),
+  password: Yup.string().required("Password is required").trim()
+});
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method != "POST") {
+    res.status(StatusCode.BAD_REQUEST).json({
+      message: "Not Found"
+    });
+  }
+  
+  try {
+    const data = <{username: string; password: string;}>req.body;
+    const validatedData = await validationSchema.validate(data);
+
+    const repo = new UserRepository();
+    const existingUser = await repo.find(validatedData.username);
+    const hashedPassword = existingUser ? existingUser.password! : "";
+    const samePassword = await compare(validatedData.password, hashedPassword);
+
+    if (!existingUser || !samePassword) {
+      return res.status(StatusCode.BAD_REQUEST).json({
+        message: "Wrong credentials"
+      });
+    }
+
+    const {password, ...user} = existingUser;
+    const token = sign({sub: existingUser.id}, process.env.SECRET_KEY!, {expiresIn: "24h"});
+    const tokenDuration = 60 * 60 * 24;
+    const authSession = cookie.serialize("_auth-tk", token, {
+      httpOnly: true,
+      maxAge: tokenDuration
+    });
+
+
+    res.setHeader("Set-Cookie", authSession);
+    res.status(StatusCode.SUCCESS).json({
+      message: "Access granted",
+      data: user
+    });
+  }
+  catch(error) {
+    let message = "Something went wrong";
+
+    if (error instanceof Yup.ValidationError) {
+      message = "Validation failed";
+    }
+
+    res.status(StatusCode.SERVER).json({ message });
+  }
+}
