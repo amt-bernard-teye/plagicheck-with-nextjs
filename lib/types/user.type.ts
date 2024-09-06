fb218f9e3f360ef38d72975f119006ac9fbd261c
Bernard Teye <bernard.teye@amalitech.com>
2024-09-06 12:04:24 +0000

    add user.type.ts

---
+++ lib/types/user.type.ts
@@ -0,0 +1,23 @@
+import { AvailabilityStatus, Role } from "@prisma/client";
+
+export type User = {
+  id?: string;
+  name: string;
+  email: string;
+  password?: string;
+  role: Role;
+  image: string | null;
+  status?: AvailabilityStatus
+  phoneNumber: string;
+}
+
+
+export type UserProp = {
+  id: boolean;
+  name: boolean;
+  email: boolean;
+  role: boolean;
+  image: boolean;
+  phoneNumber: boolean;
+  status: boolean;
+}
