7014c0a3c7c8b6fe059c7fe332a40f10a335b9be
Bernard Teye <bernard.teye@amalitech.com>
2024-09-06 13:34:57 +0000

    refactor form-control component to include hasError props

--- components/molecules/form-control/form-control.tsx
+++ components/molecules/form-control/form-control.tsx
@@ -1,19 +1,18 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 
 import styles from "./form-control.module.css";
-import SMSTracking from "@/components/atoms/icons/sms-tracking";
 
 type FormControl = {
   leftIcon?: ReactNode
   children?: ReactNode,
+  hasError: boolean;
 } & ComponentPropsWithoutRef<"input">;
 
-export default function FormControl({leftIcon, children, ...props}: FormControl) {
-
+export default function FormControl({hasError, leftIcon, children, ...props}: FormControl) {
   return (
-    <div className={styles.formControl}>
+    <div className={`${styles.formControl} ${hasError && styles.inputError}`}>
       {leftIcon}
-      <input {...props} />
+      <input {...props}/>
       { children }
     </div>
   );
