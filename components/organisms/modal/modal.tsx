6d15b9d052a4df2d5a6ee5c2f2a7a248108c3101
Bernard Teye <bernard.teye@amalitech.com>
2024-09-07 14:19:51 +0000

    add Modal component

---
+++ components/organisms/modal/modal.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+
+import Backdrop from "@/components/atoms/backdrop/backdrop";
+import styles from "./modal.module.css";
+import CloseButton from "@/components/atoms/close-button/close-button";
+
+type ModalProps = PropsWithChildren<{
+  title: string;
+  onToggle: () => void;
+}>
+
+export default function Modal({title, children, onToggle}: ModalProps) {
+  return (
+    <>
+      <Backdrop onClick={onToggle}/>
+      <section className={styles.modal}>
+        <div className="flex align-items-center justify-content-between mb-2">
+          <h3>{title}</h3>
+          <CloseButton onClick={onToggle}/>
+        </div>
+        { children }
+      </section>
+    </>
+  );
+}
