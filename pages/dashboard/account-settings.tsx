26323fd3a31027d73ea49ac3e99e5c178503244c
Bernard Teye <bernard.teye@amalitech.com>
2024-09-07 12:12:36 +0000

    add Index, ManageUsers, Archive, AccountSettings, AcademicDivision, History, Checker and Schedule pages

---
+++ pages/dashboard/account-settings.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+import Dashboard from "@/components/layouts/dashboard/dashboard";
+
+export default function AccountSettings() {
+  return (
+    <>
+      <Head>
+        <title>Plagiarism Checker | Account Settings</title>
+        <meta name="description" content="Plagiarism Checker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.svg" />
+      </Head>
+      <Dashboard>
+        <h1>Testing</h1>
+      </Dashboard>
+    </>
+  );
+}
