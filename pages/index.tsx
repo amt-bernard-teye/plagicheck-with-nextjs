aa30212d02bc756bceff4761e4059aafb7484f8b
Bernard Teye <bernard.teye@amalitech.com>
2024-09-09 09:15:36 +0000

    complete logic for login api call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
 
 import Auth from "@/components/layouts/auth/Auth";
 import FormGroup from "@/components/atoms/form-group/form-group";
@@ -13,14 +16,50 @@ import Lock from "@/components/atoms/icons/lock";
 import Alert from "@/components/molecules/alert/alert";
 import { AlertVariant } from "@/lib/enums/alert-variant";
 import { useAlert } from "@/lib/hooks/useAlert";
+import InputError from "@/components/atoms/input-error/input-error";
+import { login } from "@/lib/api/auth";
 
 export default function Home() {
+  const router = useRouter();
   const { alertDetails, handleAlertDetails } = useAlert();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("Email/Staff ID is required"),
+      password: Yup.string().required("Password is required")
+    }),
+    onSubmit: handleSubmit
+  });
 
+  
   function togglePasswordVisibility() {
     setShowPassword(prevState => !prevState);
   }
+
+  
+  async function handleSubmit(values: {username: string; password: string}) {
+    setIsSubmitting(true);
+
+    try {
+      const result = await login(values);
+      handleAlertDetails(result.message, AlertVariant.SUCCESS);
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, 2000);
+    }
+    catch(error: any) {
+      handleAlertDetails(error.message, AlertVariant.ERROR);
+    }
+    finally {
+      setIsSubmitting(false);
+    }
+  }
+
 
   return (
     <>
@@ -33,20 +72,25 @@ export default function Home() {
       <Auth 
         title="Login"
         description="Please enter your login details below to access your account">
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <FormGroup className="mb-2">
             <Label htmlFor="username">Email/Staff ID</Label>
-            <FormControl placeholder="Your email or staff ID" type="email"
-              leftIcon={<SMSTracking />}/>
+            <FormControl 
+              hasError={!!formik.errors.username && formik.touched.username!}
+              placeholder="Your email or staff ID" type="text"
+              leftIcon={<SMSTracking />} {...formik.getFieldProps("username")}/>
+            {(formik.errors.username && formik.touched.username) && <InputError message={formik.errors.username} />}
           </FormGroup>
           <FormGroup className="mb-2">
             <Label htmlFor="password">Password</Label>
             <FormControl 
+              hasError={!!formik.errors.password && formik.touched.password!}
               placeholder="Type your password here" 
               type={showPassword ? "text" : "password"}
-              leftIcon={<Lock />}>
+              leftIcon={<Lock />} {...formik.getFieldProps("password")}>
               <PasswordToggle state={showPassword} onToggle={togglePasswordVisibility}/>
             </FormControl>
+            {(formik.errors.password && formik.touched.password) && <InputError message={formik.errors.password} />}
           </FormGroup>
           <div className="flex justify-content-between mb-2">
             <Label>
@@ -55,7 +99,8 @@ export default function Home() {
             <Link href="/forgot-password" className="link">Forgot password?</Link>
           </div>
           <div className="flex flex-column">
-            <Button variant="primary">Login</Button>
+            <Button variant="primary" type="submit"
+              disabled={(!formik.values.username || !formik.values.password) || isSubmitting}>Login</Button>
           </div>
         </form>
       </Auth>
