9c54668a0df07ad5689ffba1e7ed7178c1ec8198
Bernard Teye <bernard.teye@amalitech.com>
2024-09-11 08:22:29 +0000

    add getFaculties() to faculty.ts file

--- lib/api/faculty.ts
+++ lib/api/faculty.ts
@@ -1,3 +1,4 @@
+import { FetchForm } from "../enums/fetch-form";
 import { StatusCode } from "../enums/status-code";
 
 export async function createFaculty(formData: {name: string}) {
@@ -12,6 +13,26 @@ export async function createFaculty(formData: {name: string}) {
   const result = await response.json();
 
   if (response.status !== StatusCode.CREATED) {
+    throw new Error(result.message);
+  }
+
+  return result;
+}
+
+
+export async function getFaculties(form: FetchForm = FetchForm.PAGINATE, query: string = "", page: number = 0) {
+  const params = new URLSearchParams();
+  params.append("paginate", form);
+  params.append("page", page.toString());
+  params.append("q", query);
+
+  const response = await fetch(`http://localhost:3000/api/faculties?${params.toString()}`, {
+    method: "GET",
+  });
+
+  const result = await response.json();
+
+  if (response.status !== StatusCode.SUCCESS) {
     throw new Error(result.message);
   }
 
