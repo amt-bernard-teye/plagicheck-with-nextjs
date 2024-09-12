604402a9039b2ec6fb9561317d4983d0eb5b0267
Bernard Teye <bernard.teye@amalitech.com>
2024-09-12 16:10:19 +0000

    add updateDepartment and deleteDepartment to department.ts

--- lib/api/department.ts
+++ lib/api/department.ts
@@ -1,4 +1,5 @@
 import { StatusCode } from "../enums/status-code";
+import { Department } from "../types/department.type";
 
 export async function createDepartment(values: {name: string; facultyId: number}) {
   const response = await fetch("http://localhost:3000/api/departments", {
@@ -12,6 +13,43 @@ export async function createDepartment(values: {name: string; facultyId: number}
   const result = await response.json();
 
   if (response.status !== StatusCode.CREATED) {
+    throw new Error(result.message);
+  }
+
+  return result;
+}
+
+
+export async function updateDepartment(value: Department) {
+  const response = await fetch(`http://localhost:3000/api/departments/${value.id}`, {
+    method: "PUT",
+    body: JSON.stringify(value),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const result = await response.json();
+
+  if (response.status !== StatusCode.SUCCESS) {
+    throw new Error(result.message);
+  }
+
+  return result;
+}
+
+
+export async function deleteDepartment(id: number) {
+  const response = await fetch(`http://localhost:3000/api/departments/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const result = await response.json();
+
+  if (response.status !== StatusCode.SUCCESS) {
     throw new Error(result.message);
   }
 
