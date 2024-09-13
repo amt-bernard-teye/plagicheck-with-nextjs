1a439394759538872459f3fddcffe755dbe5960c
Bernard Teye <bernard.teye@amalitech.com>
2024-09-13 16:29:16 +0000

    remove manage-user.module.css file and ui for default header and bulk actions

--- pages/dashboard/manage-users.tsx
+++ pages/dashboard/manage-users.tsx
@@ -1,9 +1,38 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 import Dashboard from "@/components/layouts/dashboard/dashboard";
 import PageHeader from "@/components/organisms/page-header/page-header";
+import Modal from "@/components/organisms/modal/modal";
+import LecturerForm from "@/components/organisms/lecturer-form/lecturer-form";
+import StudentForm from "@/components/organisms/student-form/student-form";
+import { UserTabs } from "@/lib/enums/user-tab";
+import UserDetails from "@/components/organisms/user-details/user-details";
+import BulkUpload from "@/components/organisms/bulk-upload/bulk-upload";
 
 export default function ManageUsers() {
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+  const [showBulkPane, setShowBulkPane] = useState(false);
+  
+  function setActiveTab(tab: UserTabs) {
+    const params = new URLSearchParams();
+    params.set("tab", tab);
+    router.replace(`${router.pathname}?${params.toString()}`);
+  }
+  
+  
+  function toggleBulkPane() {
+    setShowBulkPane(showBulkPane => !showBulkPane);
+  }
+  
+  
+  let browserTab = router.query["tab"] as string || "";
+  let activeTab = ["lecturer", "student"].includes(browserTab) ? browserTab : UserTabs.LECTURER;
+  let modalHeading = activeTab === UserTabs.LECTURER ? "Add Lecturer" : "Add Student";
+  let form = activeTab === UserTabs.LECTURER ? <LecturerForm /> : <StudentForm /> ;
+
   return (
     <>
       <Head>
@@ -14,7 +43,28 @@ export default function ManageUsers() {
       </Head>
       <Dashboard>
         <PageHeader />
-        <h1>Testing</h1>
+        {!showBulkPane ? (
+          <UserDetails 
+            activeTab={activeTab as UserTabs}
+            onToggleModal={() => setShowModal(!showModal)}
+            onSetActiveTab={setActiveTab}
+            onNavigateToBulk={toggleBulkPane}/>
+        ) : (
+          <BulkUpload
+            entity={activeTab === UserTabs.LECTURER ? 'Lecturers' : 'Students'}
+            onReturn={toggleBulkPane} />
+        )}
+        
+
+        {showModal && (
+          <Modal 
+            title={modalHeading} 
+            onToggle={() => {
+              setShowModal(showModal => !showModal);
+            }}>
+            { form }
+          </Modal>
+        )}
       </Dashboard>
     </>
   );
