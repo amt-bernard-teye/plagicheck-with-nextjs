12440e12b9cf01fedbe8ea66b7f9400d5f0c4866
Bernard Teye <bernard.teye@amalitech.com>
2024-10-09 09:03:59 +0000

    add Auth layout component

---
+++ components/templates/auth.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+import AppLogo from "../molecules/app-brand";
+
+type AuthProps = {
+  children: ReactNode
+}
+
+export default function Auth({children}: AuthProps) {
+  return (
+    <main className="bg-[#fafafb] w-[100%] h-[100vh] overflow-auto lg:overflow-hidden flex items-center justify-center">
+      <section className="w-[95%] p-4 md:w-[471px]  md:p-8 bg-white shadow-md rounded-2xl space-y-4">
+        <AppLogo />
+        { children }
+      </section>
+    </main>
+  );
+}
