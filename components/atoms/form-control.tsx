c9865c6eec42b92f52d32d393e1592224d7adff3
Bernard Teye <bernard.teye@amalitech.com>
2024-10-09 11:33:44 +0000

    refactor FormControl component by adding error icon component when input is invalid

--- components/atoms/form-control.tsx
+++ components/atoms/form-control.tsx
@@ -1,15 +1,18 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
+import ExclamationDanger from "./icons/exclamation-danger";
 
 type FormControlProps = {
   leftIcon?: ReactNode
   children?: ReactNode;
+  hasError?: boolean;
 } & ComponentPropsWithoutRef<"input">;
 
-export default function FormControl({leftIcon, children, ...props}: FormControlProps) {
+export default function FormControl({leftIcon, children, hasError, ...props}: FormControlProps) {
   return (
-    <div className="flex items-center gap-2 border border-[#a6a6ab] rounded-lg px-4">
+    <div className={`flex items-center gap-2 border border-[#a6a6ab] rounded-lg px-4 ${hasError && 'border-[#ff0000]'}`}>
       { leftIcon }
       <input {...props} className="py-[10px] outline-none flex-grow"/>
+      {hasError && <ExclamationDanger />}
       { children }
     </div>
   );
