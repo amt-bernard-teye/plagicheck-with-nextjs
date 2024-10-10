68214ebd3127afa4c76ce2351305e428d0738a7e
Bernard Teye <bernard.teye@amalitech.com>
2024-10-10 11:22:01 +0000

    add Profile component and PageHeader component

---
+++ components/molecules/profile.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+
+import Person from "@/components/atoms/icons/person";
+import profileImage from "@/public/profile.jpg";
+
+export default function Profile() {
+  let hasImage = false;
+  let [ _, email, name ] = document.cookie.split(";").map(value => value.split("=")[1]);
+  
+  return (
+    <div className="flex gap-4 items-center">
+      <div className="w-12 h-12 relative">
+        {!hasImage 
+          ? <div className="border border-[var(--gray-700)] rounded-full w-[inherit] h-[inherit] flex items-center justify-center"><Person /></div>
+          : <Image src={profileImage} alt="User profile image" className="w-[100%] h-[100%] object-cover rounded-full"/>
+        }
+        <span className="absolute bottom-0 right-0 w-4 h-4 bg-[var(--success-100)] inline-block rounded-full border-2 border-white"></span>
+      </div>
+      <div>
+        <h5 className="font-semibold">{ name }</h5>
+        <p className="text-[0.875em]">{email.replace("%", "@")}</p>
+      </div>
+    </div>
+  );
+}
---
+++ components/organisms/page-header.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useDebouncedCallback } from "use-debounce";
+import { ChangeEvent } from "react";
+// import { useRouter } from "next/navigation";
+
+import FormControl from "../atoms/form-control";
+import Search from "@/components/atoms/icons/search";
+import Profile from "../molecules/profile";
+
+
+type PageHeaderProps = {
+  onSearch?: (value: string) => void;
+}
+
+
+export default function PageHeader({onSearch}: PageHeaderProps) {
+  // const router = useRouter();
+  // const searchTerm = router.query["q"] as string || "";
+
+  const handleSearch = useDebouncedCallback((event: ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value || "";
+
+    if (onSearch) {
+      onSearch(value);
+    }
+    else {
+      const params = new URLSearchParams();
+      params.set("q", value);
+
+      // if (value) {
+      //   router.replace(`${router.pathname}?${params.toString()}`);
+      // } else {
+      //   router.replace(`${router.pathname}`);
+      // }
+    }
+  }, 650);
+
+
+  let searchIcon = (
+    <div>
+      <Search />
+    </div>
+  );
+
+  return (
+    <header className="flex justify-between py-[15px] px-[21px] xl:py-[19px] xl:px-[41px] border border-b-[#bcbcc0]">
+      <div className="w-[100%] lg:w-[307px]">
+        <FormControl 
+          type="search"
+          placeholder="Search anything here"
+          onChange={handleSearch}
+          // defaultValue={searchTerm}
+          leftIcon={searchIcon} />
+      </div>
+      <div className="hidden lg:block">
+        <Profile />
+      </div>
+    </header>
+  );
+}
