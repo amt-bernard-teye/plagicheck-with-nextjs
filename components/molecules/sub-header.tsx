2df5094feb8d0299f60610e1caf1a7fecc727305
Bernard Teye <bernard.teye@amalitech.com>
2024-10-15 09:17:54 +0000

    refactor SubHeader component and use Heading component

--- components/molecules/sub-header.tsx
+++ components/molecules/sub-header.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import Heading from "../atoms/heading";
 
 type SubHeaderProps = PropsWithChildren<{
   title: string;
@@ -9,7 +10,7 @@ export default function SubHeader({title, description, children}: SubHeaderProps
   return (
     <div className="flex flex-col md:flex-row md:justify-between gap-4 py-[15px] px-[21px] lg:py-[19px] lg:px-[41px] border-b border-b-[var(--gray-800)]">
       <div className="">
-        <h4 className="text-[1.2em] font-semibold">{title}</h4>
+        <Heading>{title}</Heading>
         <p>{description}</p>
       </div>
       { children }
