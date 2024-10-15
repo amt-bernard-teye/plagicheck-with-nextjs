d0de0655597b3c5c0fa570faf54964a839b977f0
Bernard Teye <bernard.teye@amalitech.com>
2024-10-15 11:12:19 +0000

    refactor rout.ts in auth/login to use the setAuthCookie function

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,12 +1,11 @@
 import { compare } from "bcryptjs";
-import { sign } from "jsonwebtoken";
-import { cookies } from "next/headers";
 import { ValidationError } from "yup";
 
 import { AppException } from "@/lib/exceptions/app.exception";
 import { UserRepository } from "@/lib/repository/user.repository";
 import { loginValidationSchema } from "@/lib/validation/login.validation";
 import { StatusCode } from "@/lib/enum/status-code";
+import { setAuthCookie } from "@/lib/util/setup-cookie.util";
 
 
 export async function POST(req: Request) {
@@ -23,18 +22,7 @@ export async function POST(req: Request) {
       throw new AppException("Wrong credentials");
     }
 
-    const {password, ...user} = existingUser;
-    const token = sign({id: existingUser.id, email: existingUser.email}, process.env.SECRET_KEY!, {expiresIn: "24h"});
-    const tokenDuration = 60 * 60 * 24 * 1000;
-    
-    cookies().set("_auth-tk", token, {
-      httpOnly: true,
-      maxAge: tokenDuration,
-      path: "/"
-    });
-    cookies().set("user_id", user.id!, { maxAge: tokenDuration, path: "/" });
-    cookies().set("user_email", user.email, { maxAge: tokenDuration, path: "/" });
-    cookies().set("user_name", user.name, { maxAge: tokenDuration, path: "/" });
+    setAuthCookie(existingUser);
     
     return Response.json({
       message: "Access granted",
