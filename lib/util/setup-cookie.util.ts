4d0fdd8b08c6fc7469d137f9a5811fbb527323d6
Bernard Teye <bernard.teye@amalitech.com>
2024-10-15 11:08:19 +0000

    add setup-cookie.util.ts containing setAuthCookie()

---
+++ lib/util/setup-cookie.util.ts
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+import { sign } from "jsonwebtoken";
+
+import { User } from "../types/user.type";
+
+export function setAuthCookie(existingUser: User) {
+  const {password, ...user} = existingUser;
+  const token = sign({id: existingUser.id, email: existingUser.email}, process.env.SECRET_KEY!, {expiresIn: "24h"});
+  const tokenDuration = 60 * 60 * 24 * 1000;
+  
+  cookies().set("_auth-tk", token, {
+    httpOnly: true,
+    maxAge: tokenDuration,
+    path: "/"
+  });
+  cookies().set("user_id", user.id!, { maxAge: tokenDuration, path: "/" });
+  cookies().set("user_email", user.email, { maxAge: tokenDuration, path: "/" });
+  cookies().set("user_name", user.name, { maxAge: tokenDuration, path: "/" });
+}
