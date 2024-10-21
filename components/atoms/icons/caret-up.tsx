9b26db154c57a4d98c4c1a0b43ccc9658ed20fa4
Bernard Teye <bernard.teye@amalitech.com>
2024-10-21 14:09:15 +0000

    add CaretDown and CaretUp component

---
+++ components/atoms/icons/caret-up.tsx
@@ -0,0 +1,7 @@
+export default function CaretUp() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19.9201 15.05L13.4001 8.53001C12.6301 7.76001 11.3701 7.76001 10.6001 8.53001L4.08008 15.05" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
