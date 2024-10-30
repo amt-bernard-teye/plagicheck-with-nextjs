35f1cfdbcc3fa7277fcf00679f56dda4931fc604
Bernard Teye <bernard.teye@amalitech.com>
2024-10-30 09:58:54 +0000

    refactor departmentValidationSchema in academic-division.validation.ts

--- lib/validation/academic-divisition.validation.ts
+++ lib/validation/academic-divisition.validation.ts
@@ -5,8 +5,15 @@ const facultyValidationSchema = Yup.object({
 });
 
 const departmentValidationSchema = Yup.object({
-  name: Yup.string().required("Name is required").matches(/^[a-zA-Z ]*$/, "Only letters and whitespaces are allowed").trim(),
-  categoryId: Yup.string().required("Category ID is required").matches(/^[0-9]*$/, "Only numbers are allowed").trim(),
+  name: Yup.string().required("Department name is required")
+    .matches(/^[a-zA-Z ]*$/, "Only letters and white spaces are allowed")
+    .min(3, "Must be at least 3 characters")
+    .max(50, "Must be 30 characters or less")
+    .trim(),
+  facultyId: Yup.string()
+    .required("Faculty ID is required")
+    .matches(/^[0-9]*$/, "Only numbers are allowed")
+    .trim(),
 });
 
 export { facultyValidationSchema, departmentValidationSchema };
