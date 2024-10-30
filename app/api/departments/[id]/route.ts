3d029dffe40db4a1967334c2d3fcc1b1a6fdc8b2
Bernard Teye <bernard.teye@amalitech.com>
2024-10-30 10:17:54 +0000

    add delete route logic for removing department

--- app/api/departments/[id]/route.ts
+++ app/api/departments/[id]/route.ts
@@ -2,7 +2,6 @@ import { StatusCode } from "@/lib/enum/status-code";
 import { AppException } from "@/lib/exceptions/app.exception";
 import { getExceptionMessage } from "@/lib/exceptions/exception-handler";
 import { DepartmentRepository } from "@/lib/repository/department.repository";
-import { FacultyRepository } from "@/lib/repository/faculty.repository";
 import { checkFaculty } from "@/lib/util/check-entities.util";
 import { departmentValidationSchema } from "@/lib/validation/academic-divisition.validation";
 
@@ -30,6 +29,32 @@ export async function PUT(req: Request, {params}: PutParams) {
     existingDepartment.name = validatedData.name;
     existingDepartment.facultyId = existingFaculty.id;
     const updatedDepartment = await departmentRepo.update(existingDepartment);
+
+    return Response.json({
+      message: "Department updated successfully",
+      data: updatedDepartment
+    });
+  }
+  catch(error) {
+    let message = getExceptionMessage(error);
+    return Response.json({ message }, {
+      status: StatusCode.SERVER
+    });
+  }
+}
+
+
+export async function DELETE(req: Request, {params}: PutParams) {
+  const { id } = params;
+
+  try {
+    const departmentRepo = new DepartmentRepository();
+    const existingDepartment = await departmentRepo.find(+id);
+
+    if (!existingDepartment) {
+      throw new AppException("Department doesn't exist");
+    }
+    const updatedDepartment = await departmentRepo.delete(existingDepartment.id!);
 
     return Response.json({
       message: "Department updated successfully",
