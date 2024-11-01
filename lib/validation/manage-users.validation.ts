94aa74b8169a97f98d62b9526e672f2b70e126cb
Bernard Teye <bernard.teye@amalitech.com>
2024-11-01 15:16:58 +0000

    add studentValidationSchema to manage-users.validation.ts

--- lib/validation/manage-users.validation.ts
+++ lib/validation/manage-users.validation.ts
@@ -9,4 +9,12 @@ const lecturerValidationSchema = Yup.object({
 });
 
 
-export { lecturerValidationSchema };
+const studentValidationSchema = Yup.object({
+  name: Yup.string().required().matches(/^[a-zA-Z ]*$/),
+  email: Yup.string().required().email(),
+  phoneNumber: Yup.string().required().matches(/^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/),
+  departmentId: Yup.string().required().matches(/^[0-9]/)
+});
+
+
+export { lecturerValidationSchema, studentValidationSchema };
