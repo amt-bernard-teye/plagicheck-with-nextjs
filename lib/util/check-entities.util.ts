e7a1b2e9f3477c304a69943b59ac6eb276cddce6
Bernard Teye <bernard.teye@amalitech.com>
2024-11-01 13:56:55 +0000

    add checkDepartment and ensureEmailNeverExist functions to check-entities

--- lib/util/check-entities.util.ts
+++ lib/util/check-entities.util.ts
@@ -1,9 +1,10 @@
 import { AppException } from "../exceptions/app.exception";
+import { DepartmentRepository } from "../repository/department.repository";
 import { FacultyRepository } from "../repository/faculty.repository";
+import { UserRepository } from "../repository/user.repository";
 
 export async function checkFaculty(facultyId: number) {
   const facultyRepo = new FacultyRepository();
-
   const existingFaculty = await facultyRepo.find(facultyId);
 
   if (!existingFaculty) {
@@ -11,4 +12,26 @@ export async function checkFaculty(facultyId: number) {
   }
 
   return existingFaculty;
+}
+
+
+export async function checkDepartment(departmentId: number) {
+  const departmentRepo = new DepartmentRepository();
+  const department = await departmentRepo.find(departmentId);
+
+  if (!department) {
+    throw new Error("Department doesn't exist");
+  }
+
+  return department;
+}
+
+
+export async function ensureEmailNeverExist(email: string) {
+  const userRepo = new UserRepository();
+  const existingUser = await userRepo.find(email);
+
+  if (existingUser) {
+    throw new Error("Email already exist, please try again with another one");
+  }
 }
